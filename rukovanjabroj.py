dc248d4671692e010240b8ec508fbddb2b5111e3
MarkoMatijasevic <97795056+MarkoMatijasevic@users.noreply.github.com>
2022-04-13 10:28:55 +0200

    Add files via upload

---
+++ rukovanjabroj.py
@@ -0,0 +1,26 @@
+# def get_participants(handshakes):
+#     return handshakes*(handshakes-1)/2
+
+# print(int(get_participants(8)))
+
+
+
+#resenje zadatka sa codewarsa
+
+# def get_participants(handshakes, n = 1):
+#     return get_participants(handshakes, n + 1) if (n * n - n)/2 < handshakes else n
+
+
+
+
+
+# broj = int(input("Unesi broj stranica: "))
+# skup=[]
+# for i in range (1,broj):
+#     skup.append(i)
+
+# print(str(skup))
+
+
+
+
